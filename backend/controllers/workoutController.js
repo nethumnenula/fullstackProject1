5baa98198125c41ee3189f4232cb829b48e6c845
Nethum <nethumnenula2004@gmail.com>
2026-07-13 01:27:02 +0530

    Added Controller functions to get workouts

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,37 +1,51 @@
-const Workout = require('../models/workoutModel');
+const Workout = require("../models/workoutModel");
+const mongoose = require('mongoose');
 
 // GET all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await Workout.find({}).sort({createdAt: -1})
-}
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
 
+  res.status(200).json(workouts);
+};
 
 // GET a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
 
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error: "No such workouts"});
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
 
 // create a new workout
 const createWorkout = async (req, res) => {
-    const {title, load, reps} = req.body; 
+  const { title, load, reps } = req.body;
 
-    //add doc to db
-  try{
-    const workout = await Workout.create({title, load, reps});
+  //add doc to db
+  try {
+    const workout = await Workout.create({ title, load, reps });
     res.status(200).json(workout);
-
-  } catch (error){
-    res.status(400).json({error: error.message})
-  } 
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 
   //res.json({ mssg: "POST a new workout" });
-}
-
+};
 
 // delete a workout
 
-
 // update a workout
 
-
 module.exports = {
-    createWorkout
-}
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+};
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const { createWorkout } = require("../controllers/workoutController");
+const { 
+    createWorkout,
+    getWorkouts,
+    getWorkout
+ } = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ mssg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 //GET a single workout
-router.get("/:id", (req, res) => {
-  res.json({ mssg: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 //POST a new workout
 router.post("/", createWorkout);
