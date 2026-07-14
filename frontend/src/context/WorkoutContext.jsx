42ab29b2115ebc26ba7a8c9c8e5d9df0ffbd4c31
Nethum <nethumnenula2004@gmail.com>
2026-07-14 22:51:16 +0530

    Delete Button added

--- frontend/src/context/WorkoutContext.jsx
+++ frontend/src/context/WorkoutContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { act, createContext, useReducer, useState } from "react";
 
 export const WorkoutsContext = createContext();
 
@@ -12,6 +12,11 @@ export const workoutsReducer = (state, action) => {
       return {
         workouts: [action.payload, ...state.workouts],
       };
+
+    case "DELETE_WORKOUT":
+        return{
+            workouts: state.workouts.filter((w) => w._id !== action.payload._id)
+        }
     default:
       return state;
   }
